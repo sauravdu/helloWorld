6a2939932c5d89ece9f591725b22672bff2dbc25
An <anishamalhotra@Anishas-MacBook-Pro.local>
2020-04-30 11:37:52 -0700

    hello world

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Anisha Malhotra on 4/30/20.
+//  Copyright © 2020 Anisha Malhotra. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
